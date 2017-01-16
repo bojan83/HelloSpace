643575a887ae7da96d56d3340aaf60306bb50d6f
bojan83 <njublok@yahoo.com>
2017-01-16 13:49:33 +0100

    Added a comment in main activity and made a new class called Test

--- app/src/main/java/com/bignerdranch/android/hellospace/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/hellospace/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Look this is the leyout file i am linking
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/hellospace/Test.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.hellospace;
+
+/**
+ * Created by bojan on 1/16/2017.
+ */
+
+public class Test {
+    public String name;
+}
